45563d49ab93d0c26f04caf82d57e2aacf6e0ba2
Opdata <pb03641@gmail.com>
2019-12-05 02:52:16 +0900

    HTML 에 web3(IPFS), JS(정적) link 완료, Chat 컨트랙트 작성 및 배포 완료, fun.js(web3 연동완료)

--- contracts/Chat.sol
+++ contracts/Chat.sol
@@ -1,5 +1,16 @@
 pragma solidity >=0.4.25 <0.6.0;
 
 contract Chat {
-//Edit..
+    uint256 numberOfLine;
+    struct TextBody{
+        string name;
+        string Text;
+    }
+    
+    TextBody[] public content;
+    
+    function pushText(string memory _name, string memory _text) public {
+        content.push(TextBody(_name, _text));
+        numberOfLine++;
+    }
 }
--- view/fun.js
+++ view/fun.js
@@ -1,5 +1,25 @@
-function getName() {}
+function initweb3() {
+  if (typeof web3 !== "undefined") {
+    web3 = new Web3(web3.currentProvider);
+  } else {
+    // set the provider you want from Web3.providers
+    web3 = new Web3(new Web3.providers.HttpProvider("http://localhost:7545"));
+  }
 
-function MessageSubmit() {}
+  if (web3.isConnected()) {
+    console.log("connected");
+  } else {
+    console.log("not connected");
+    exit;
+  }
+}
 
-function addMessageBody() {}
+// You should initialize web3 instance after window load event has fired to avoid any race condition.
+
+window.addEventListener("load", initweb3);
+
+// function getName() {}
+
+// function MessageSubmit() {}
+
+// function addMessageBody() {}
