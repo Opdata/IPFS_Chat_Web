1c4729d7e43a8c226a516257422a4ef62104c288
Opdata <pb03641@gmail.com>
2019-12-05 16:02:01 +0900

    [feat] 컨트랙트로드 기능, 메시지전송 기능, 전체채팅로드 기능, 추가메시지로드 기능 추가

--- view/fun.js
+++ view/fun.js
@@ -1,8 +1,12 @@
+var web3;
+let contractAddr;
+let vc;
+
 function initweb3() {
+  //완성
   if (typeof web3 !== "undefined") {
     web3 = new Web3(web3.currentProvider);
   } else {
-    // set the provider you want from Web3.providers
     web3 = new Web3(new Web3.providers.HttpProvider("http://localhost:7545"));
   }
 
@@ -12,14 +16,50 @@ function initweb3() {
     console.log("not connected");
     exit;
   }
+
+  function loadContract() {
+    contractAddr = "0xdB5e260c3A17F58b046Cd8F435E00bfaA893b735";
+    vc = web3.eth.contract(abi).at(contractAddr);
+  }
+  loadContract();
+  showMessage();
+  return [contractAddr, vc, web3];
+}
+[contractAddr, vc, web3] = initweb3();
+
+function messageSubmit() {
+  //완성
+  var account = web3.eth.accounts[0];
+  var message = document.getElementById("text-message").value;
+  document.getElementById("text-message").value = "";
+  vc.pushText(message, { from: account, gas: 2000000 }, function(err, result) {
+    if (!err) {
+      console.log("메시지 정상 등록");
+    } else {
+      console.log("메시지 등록 실패");
+    }
+  });
+  addshowMessage();
 }
 
-// You should initialize web3 instance after window load event has fired to avoid any race condition.
+function showMessage() {
+  var table = document.getElementById("table1");
+  var length = vc.getLine()["c"][0];
+  for (var i = 0; i < length; i++) {
+    var message = vc.getText(i);
+    var strArray = message.toString();
+    var row = table.insertRow();
+    var cell1 = row.insertCell(0);
+    cell1.innerHTML = strArray;
+  }
+}
 
-window.addEventListener("load", initweb3);
-
-// function getName() {}
-
-// function MessageSubmit() {}
-
-// function addMessageBody() {}
+function addshowMessage() {
+  var table = document.getElementById("table1");
+  var length = vc.getLine()["c"][0];
+  var message = vc.getText(length - 1);
+  var strArray = message.toString();
+  var row = table.insertRow();
+  var cell1 = row.insertCell(0);
+  cell1.innerHTML = strArray;
+}
