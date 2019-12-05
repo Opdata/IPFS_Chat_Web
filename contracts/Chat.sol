0251496b3fa6660ba2613264356b7116c7d0f575
Opdata <pb03641@gmail.com>
2019-12-05 15:38:42 +0900

    [feat] 닉네임 제거, 단순 텍스트만 기록

--- contracts/Chat.sol
+++ contracts/Chat.sol
@@ -1,16 +1,37 @@
-pragma solidity >=0.4.25 <0.6.0;
+pragma solidity ^0.5.11;
 
 contract Chat {
     uint256 numberOfLine;
-    struct TextBody{
-        string name;
+	// Add Name Version
+    // struct TextBody{
+    //     string name;
+    //     string Text;
+    // }
+	struct TextBody{
         string Text;
     }
     
     TextBody[] public content;
     
-    function pushText(string memory _name, string memory _text) public {
-        content.push(TextBody(_name, _text));
+    // function pushText(string memory _name, string memory _text) public {
+    //     content.push(TextBody(_name, _text));
+    //     numberOfLine++;
+    // }
+
+	// function getText(uint256 _index) public view returns(string memory, string memory){
+	// 	return (content[_index].name, content[_index].Text);
+	// }
+
+	function pushText(string memory _text) public {
+        content.push(TextBody(_text))-1;
         numberOfLine++;
     }
+
+	function getText(uint256 _index) public view returns(string memory){
+		return (content[_index].Text);
+	}
+
+	function getLine() public view returns(uint256){
+		return numberOfLine;
+	}
 }
---
+++ view/abi.js
@@ -0,0 +1,74 @@
+const abi = [
+  {
+    constant: false,
+    inputs: [
+      {
+        internalType: "string",
+        name: "_text",
+        type: "string"
+      }
+    ],
+    name: "pushText",
+    outputs: [],
+    payable: false,
+    stateMutability: "nonpayable",
+    type: "function"
+  },
+  {
+    constant: true,
+    inputs: [
+      {
+        internalType: "uint256",
+        name: "_index",
+        type: "uint256"
+      }
+    ],
+    name: "getText",
+    outputs: [
+      {
+        internalType: "string",
+        name: "",
+        type: "string"
+      }
+    ],
+    payable: false,
+    stateMutability: "view",
+    type: "function"
+  },
+  {
+    constant: true,
+    inputs: [
+      {
+        internalType: "uint256",
+        name: "",
+        type: "uint256"
+      }
+    ],
+    name: "content",
+    outputs: [
+      {
+        internalType: "string",
+        name: "Text",
+        type: "string"
+      }
+    ],
+    payable: false,
+    stateMutability: "view",
+    type: "function"
+  },
+  {
+    constant: true,
+    inputs: [],
+    name: "getLine",
+    outputs: [
+      {
+        internalType: "uint256",
+        name: "",
+        type: "uint256"
+      }
+    ],
+    payable: false,
+    stateMutability: "view",
+    type: "function"
+  }
+];
